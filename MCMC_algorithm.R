4d860c30b3ad754d8abc7ffb646fcbe572d982d2
Laura Fennelly <lauramariefennelly@gmail.com>
2021-07-14 10:06:26 +0100

    Works with dummy map, sq grid, and hex grid

--- MCMC_algorithm.R
+++ MCMC_algorithm.R
@@ -23,32 +23,37 @@ f.Jp = function(G,district,Ndist,pop_ideal){
   
 }
 ############################ SETUP THE NETWORK ################################
-grid = 0
-n = 3 # grid size
-nodes = n^2
+# select grid = 0,1,2 for real map, sq, hex respectively.
+grid = 2
+n = 50 # grid size
+if (grid == 0) {
+  nodes = 26
+} else {
+  nodes = n^2
+}
 votes = c(rep(1,ceiling(0.4*nodes)),rep(0,floor(0.6*nodes)))
 # Assign an initial partition.
 Ndist = 3
 district0 = rep(Ndist,nodes)
-district0 = replace(district0, 1:(Ndist*round(nodes/Ndist,0)),
-                 rep(1:Ndist, each=(nodes/Ndist)))
+district0 = replace(district0, 1:(Ndist*floor(nodes/Ndist)),
+                    rep(1:Ndist, each=(nodes/Ndist)))
 P.data = data.frame(precinct = 1:nodes, population = rep(1, nodes), 
                     votes_blue = votes, votes_red = as.integer(!votes),
                     district = district0)
-P.data
 
 if (grid == 0) {
-  v1 = c(1,2,2,3,4,4,5,6,7,8)
-  v2 = c(2,3,4,5,6,5,7,7,8,9)
-  E.data = data.frame(v1,v2)
+  realmap = read.csv("Data/realwarddata.csv",header=T)
+  realmap=as.matrix(realmap[,2:27])
+  adjm = realmap
+} else if (grid == 1) {
+  adjm = f.adjm.sq(n)
 } else {
-  adjm = f.adjm(n)
-  E.data = f.edges(adjm)
+  adjm = f.adjm.hex(n)
 }
-E.data
+E.data = f.edges(adjm)
 class(E.data)
-E.data$length = rep(1, dim(E.data)[1])
-E.data
+E.data$weight = rep(1, dim(E.data)[1])
+
 
 # I now have two data frames - one containing precinct data and one containing 
 # edge connections and edge lengths.
@@ -57,13 +62,22 @@ g = graph_from_data_frame(E.data, directed = FALSE,
                           vertices=P.data)
 district_plotting = make_clusters(g,membership = V(g)$district)
 class(district_plotting)
-district_plotting
+
 # Setup the plotting information
+V(g)$size = 10
 if (n > 20) {
   V(g)$size = 1 # Reduce size of the nodes
   V(g)$label = NA # Do not print labels
 }
-if (grid == 1) {
+
+if (grid == 0) {
+  graph_attr(g,"layout") = layout_with_graphopt(g, charge=0.0001, mass=30,
+                                                spring.length = 0,
+                                                spring.constant = 1)
+  # layout_with_fr is another option worth exploring
+  # layout does not always give nice results - might to to do several iterations
+  # to get a good one. That completely avoids overlaps.
+} else {
   graph_attr(g,"layout") = layout_on_grid(g, width = n, height = n, dim = 2)
 }
 
@@ -71,7 +85,6 @@ graph_attr(g,"margin") = rep(0.01,4)
 par(mar=c(0.5,0.5,0.5,0.5)+0.1)
 plot(g, vertex.color=get.vertex.attribute(g,"district"), 
      vertex.frame.color=get.vertex.attribute(g,"district"))
-
 # The edgelist won't change so create it now
 Elist = get.edgelist(g)
 class(Elist) = "numeric"
@@ -80,15 +93,17 @@ for (i in 1:dim(E.data)[1]) {
   E(g)$p1[i] = V(g)$district[Elist[i,1]]
   E(g)$p2[i] = V(g)$district[Elist[i,2]]
 }
+
 ################################# CONSTANTS ###################################
 # population constants
 pop_ideal = sum(vertex_attr(g,"population"))/Ndist
-deviation = 0.8
+deviation = 0.3
 ubound = pop_ideal+(deviation*pop_ideal/2)
 lbound = pop_ideal-(deviation*pop_ideal/2)
 pop_bound = c(lbound,ubound); pop_bound
 ############################## BOUNDARY FLIP ##################################
-plot(g, vertex.color=V(g)$district, vertex.frame.color=V(g)$district)
+plot(g, vertex.color=V(g)$district, vertex.frame.color=V(g)$district,
+     edge.color=get.edge.attribute(g,"p1"))
 beta=1
 # step 1 - vector of conflicting edges
 conflict_x = which(get.edge.attribute(g,"p1") != get.edge.attribute(g,"p2"))
